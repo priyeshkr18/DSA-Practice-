f79316632f6b15595a32100ebb3e9ae7955ff107
Priyesh Choudhary <143314183+priyeshkr18@users.noreply.github.com>
2026-09-09 09:58:06 +0530

    Add files via upload

--- SearchInLinkedList.java
+++ SearchInLinkedList.java
@@ -1,3 +1,29 @@
+// import java.util.LinkedList;
+// import java.util.Scanner;
+
+// public class SearchInLinkedList {
+// 	public static void main(String[] args) {
+
+// 		Scanner scanner = new Scanner(System.in);
+
+// 		LinkedList<Integer> list = new LinkedList<>();
+
+// 		list.add(10);
+// 		list.add(20);
+// 		list.add(30);
+// 		list.add(40);
+// 		list.add(50);
+
+// 		System.out.print("Enter number to search: ");
+// 		int num = scanner.nextInt();
+
+// 		if (list.contains(num)) {
+// 			System.out.println("Element found!");
+// 		} else {
+// 			System.out.println("Element not found!");
+// 		}
+// 	}
+// }
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -17,8 +43,10 @@ public class SearchInLinkedList {
 		System.out.print("Enter number to search: ");
 		int num = sc.nextInt();
 
-		if (list.contains(num)) {
-			System.out.println("Element found!");
+		int index = list.indexOf(num);
+
+		if (index != -1) {
+			System.out.println("Element found at index: " + index);
 		} else {
 			System.out.println("Element not found!");
 		}
