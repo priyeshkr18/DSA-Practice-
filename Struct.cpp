7686f52a8521a4d974a6fee17b89a3391246be2f
Priyesh Choudhary <143314183+priyeshkr18@users.noreply.github.com>
2026-09-16 09:50:53 +0530

    Add files via upload

--- Struct.cpp
+++ Struct.cpp
@@ -1,5 +1,55 @@
-// #include <bits/stdc++.h> //struct is a real world entity
+// // // #include <bits/stdc++.h> //struct is a real world entity
+// // // using namespace std;
+// // // struct Student
+// // // {
+// // // 	int rollNo;
+// // // 	string name;
+// // // 	int age;
+// // // 	float marks;
+// // // };
+
+// // // int main()
+// // // {
+// // // 	Student s1;
+// // // 	s1.rollNo = 25;
+// // // 	s1.name = "Priyesh";
+// // // 	s1.age = 24;
+// // // 	s1.marks = 25.5;
+// // // 	cout << s1;
+// // // }
+// // #include <iostream>
+// // #include <string>
+// // using namespace std;
+
+// // struct Student
+// // {
+// // 	int rollNo;
+// // 	string name;
+// // 	int age;
+// // 	float marks;
+// // };
+
+// // int main()
+// // {
+// // 	Student s1;
+
+// // 	s1.rollNo = 25;
+// // 	s1.name = "Priyesh";
+// // 	s1.age = 24;
+// // 	s1.marks = 25.5;
+
+// // 	cout << "Roll No: " << s1.rollNo << endl;
+// // 	cout << "Name: " << s1.name << endl;
+// // 	cout << "Age: " << s1.age << endl;
+// // 	cout << "Marks: " << s1.marks << endl;
+
+// // 	return 0;
+// // }
+
+// #include <iostream>
+// #include <string>
 // using namespace std;
+
 // struct Student
 // {
 // 	int rollNo;
@@ -11,11 +61,26 @@
 // int main()
 // {
 // 	Student s1;
-// 	s1.rollNo = 25;
-// 	s1.name = "Priyesh";
-// 	s1.age = 24;
-// 	s1.marks = 25.5;
-// 	cout << s1;
+
+// 	cout << "Enter Roll No: ";
+// 	cin >> s1.rollNo;
+
+// 	cout << "Enter Name: ";
+// 	cin >> s1.name;
+
+// 	cout << "Enter Age: ";
+// 	cin >> s1.age;
+
+// 	cout << "Enter Marks: ";
+// 	cin >> s1.marks;
+
+// 	cout << "\n--- Student Details ---" << endl;
+// 	cout << "Roll No: " << s1.rollNo << endl;
+// 	cout << "Name: " << s1.name << endl;
+// 	cout << "Age: " << s1.age << endl;
+// 	cout << "Marks: " << s1.marks << endl;
+
+// 	return 0;
 // }
 #include <iostream>
 #include <string>
@@ -33,11 +98,23 @@ int main()
 {
 	Student s1;
 
-	s1.rollNo = 25;
-	s1.name = "Priyesh";
-	s1.age = 24;
-	s1.marks = 25.5;
+	// Take input
+	cout << "Enter Roll No: ";
+	cin >> s1.rollNo;
 
+	cin.ignore(); // Clear leftover Enter key
+
+	cout << "Enter Name: ";
+	getline(cin, s1.name);
+
+	cout << "Enter Age: ";
+	cin >> s1.age;
+
+	cout << "Enter Marks: ";
+	cin >> s1.marks;
+
+	// Print details
+	cout << "\n--- Student Details ---" << endl;
 	cout << "Roll No: " << s1.rollNo << endl;
 	cout << "Name: " << s1.name << endl;
 	cout << "Age: " << s1.age << endl;
